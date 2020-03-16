f0f5eee7ba55720c519b049db699a7c6b69d56fa
agvdndor <arne.vandendorpe@ugent.be>
2020-03-16 14:41:31 +0100

    added script to choose gpu that is not busy

---
+++ lib/utils/select_gpu.py
@@ -0,0 +1,7 @@
+import os
+import numpy as np
+
+def get_freer_gpu():
+    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
+    return np.argmax(memory_available)
