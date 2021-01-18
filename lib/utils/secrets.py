a88b9e8c5401f31d78122699b8d700c8b6f14238
Joppe Massant <joppe.massant@ml6.eu>
2021-01-18 15:43:27 +0100

    add back secrets

---
+++ lib/utils/secrets.py
@@ -0,0 +1,2 @@
+oauth_acces_token = "xoxp-697999164900-700324568775-692152157233-071a4fdfc681c54ee13284d666daf811"
+bot_user_oauth_acces_token = "xoxb-697999164900-700640456246-8PEzRBtCRJ2Ml5RdzgxmkmYq"
